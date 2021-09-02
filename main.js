b9596c07d9e3d289ccd66df9b02fcff9a065f593
Furkan Çetintaş <66469657+furkancetintas@users.noreply.github.com>
2021-09-02 23:56:51 +0300

    scroll reveal update

--- main.js
+++ main.js
@@ -84,3 +84,88 @@ $('.slider2').owlCarousel({
     },
   },
 });
+
+ScrollReveal().reveal('header', {
+  origin: 'top',
+  duration: 3000,
+  distance: '3rem',
+  delay: 100,
+});
+ScrollReveal().reveal('.showcase-heading', {
+  origin: 'left',
+  duration: 3000,
+  distance: '3rem',
+  delay: 300,
+});
+ScrollReveal().reveal('.latest-movie-heading', {
+  origin: 'left',
+  duration: 3000,
+  distance: '3rem',
+  delay: 300,
+});
+ScrollReveal().reveal('.movies-heading', {
+  origin: 'left',
+  duration: 3000,
+  distance: '3rem',
+  delay: 500,
+});
+ScrollReveal().reveal('.showcase', {
+  origin: 'left',
+  duration: 3000,
+  distance: '2rem',
+  delay: 2000,
+});
+ScrollReveal().reveal('.latest-movie', {
+  origin: 'left',
+  duration: 3000,
+  distance: '2rem',
+  delay: 3500,
+});
+ScrollReveal().reveal('.bx-1', {
+  origin: 'top',
+  duration: 3000,
+  distance: '2rem',
+  delay: 1000,
+});
+ScrollReveal().reveal('.bx-2', {
+  origin: 'top',
+  duration: 3000,
+  distance: '2rem',
+  delay: 1500,
+});
+ScrollReveal().reveal('.bx-3', {
+  origin: 'top',
+  duration: 3000,
+  distance: '2rem',
+  delay: 2000,
+});
+ScrollReveal().reveal('.bx-4', {
+  origin: 'top',
+  duration: 3000,
+  distance: '2rem',
+  delay: 2500,
+});
+ScrollReveal().reveal('.bx-5', {
+  origin: 'top',
+  duration: 3000,
+  distance: '2rem',
+  delay: 2500,
+});
+ScrollReveal().reveal('.bx-6', {
+  origin: 'top',
+  duration: 3000,
+  distance: '2rem',
+  delay: 3000,
+});
+ScrollReveal().reveal('.bx-7', {
+  origin: 'top',
+  duration: 3000,
+  distance: '2rem',
+  delay: 3500,
+});
+ScrollReveal().reveal('.bx-8', {
+  origin: 'top',
+  duration: 3000,
+  distance: '2rem',
+  delay: 4000,
+});
